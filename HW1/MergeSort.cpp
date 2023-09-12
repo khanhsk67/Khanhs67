78f583a9fc52068efb232273da3409282ea082b2
khanhs67 <144221150+khanhs67@users.noreply.github.com>
2023-09-12 22:00:09 +0700

    Add files via upload

---
+++ HW1/MergeSort.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+void merge(vector<int>& arr, int left, int middle, int right) {
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+
+    vector<int> leftArray(n1);
+    vector<int> rightArray(n2);
+
+
+    for (int i = 0; i < n1; i++) {
+        leftArray[i] = arr[left + i];
+    }
+    for (int i = 0; i < n2; i++) {
+        rightArray[i] = arr[middle + 1 + i];
+    }
+
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while (i < n1 && j < n2) {
+        if (leftArray[i] <= rightArray[j]) {
+            arr[k] = leftArray[i];
+            i++;
+        } else {
+            arr[k] = rightArray[j];
+            j++;
+        }
+        k++;
+    }
+
+
+    while (i < n1) {
+        arr[k] = leftArray[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = rightArray[j];
+        j++;
+        k++;
+    }
+}
+
+
+void mergeSort(vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int middle = left + (right - left) / 2;
+
+
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+
+
+        merge(arr, left, middle, right);
+    }
+}
